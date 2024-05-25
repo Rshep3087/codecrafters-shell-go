b623daa3ad544f970360407e90ba9d56617e167f
Ryan Sheppard <rshep3087@gmail.com>
2024-05-24 21:14:17 -0500

    submit

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -4,6 +4,8 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
@@ -14,7 +16,28 @@ func repl() {
 	for {
 		fmt.Fprint(os.Stdout, "$ ")
 		input, _ := bufio.NewReader(os.Stdin).ReadString('\n')
-		input = input[:len(input)-1]
-		fmt.Fprintf(os.Stdout, "%s: command not found\n", input)
+		cmd := parse(input)
+		execute(cmd)
+	}
+}
+
+type command struct {
+	name string
+	args []string
+}
+
+func parse(input string) command {
+	cmds := strings.Fields(input)
+	return command{cmds[0], cmds[1:]}
+}
+
+func execute(cmd command) {
+	switch cmd.name {
+	case "exit":
+		// get the exit code from the first argument
+		code, _ := strconv.Atoi(cmd.args[0])
+		os.Exit(code)
+	default:
+		fmt.Fprintf(os.Stdout, "%s: command not found\n", cmd.name)
 	}
 }
