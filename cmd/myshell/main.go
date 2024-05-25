c1e7ca04d3118194c5ad09b74e99b143370ad38c
Ryan Sheppard <rshep3087@gmail.com>
2024-05-24 21:14:59 -0500

    submit

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -12,6 +12,8 @@ func main() {
 	repl()
 }
 
+// repl stands for Read-Eval-Print-Loop
+// It reads input from the user, evaluates it, prints the result, and loops back to the start
 func repl() {
 	for {
 		fmt.Fprint(os.Stdout, "$ ")
@@ -21,16 +23,19 @@ func repl() {
 	}
 }
 
+// command represents a command that the user wants to execute
 type command struct {
 	name string
 	args []string
 }
 
+// parse takes a string and returns a command
 func parse(input string) command {
 	cmds := strings.Fields(input)
 	return command{cmds[0], cmds[1:]}
 }
 
+// execute takes a command and executes it
 func execute(cmd command) {
 	switch cmd.name {
 	case "exit":
